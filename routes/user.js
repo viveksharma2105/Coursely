7cd82bd8e4121a60f29821314658733597f2d059
viveksharma2105 <viveksharma02005@gmail.com>
2025-05-24 18:08:53 +0530

    complete signin endpoint with zod

--- routes/user.js
+++ routes/user.js
@@ -3,22 +3,39 @@
 //---------OR----------------
 const {Router} = require("express");
 const {userModel} = require("../db")
+const {z} = require("zod")
 const userRouter = Router();
 
+userRouter.post("/signup", async function (req, res) {
+    const reqBody = z.object({
+        email: z.string().min(3).max(100).email(),
+        password: z.string().min(4),
+        firstName: z.string().min(3),
+        lastName: z.string().min(3).max(8)
+    });
 
-userRouter.post("/signup", async function (req, res){
-    const {email, password, firstName, lastName} = req.body;
-   await userModel.create({
-        email: email,
-        password: password,
-        firstName: firstName,
-        lastName: lastName
-    })
+    try {
+           const parsedBody = reqBody.parse(req.body);
+        
+        const newUser = await userModel.create({
+            email: parsedBody.email,
+            password: parsedBody.password,
+            firstName: parsedBody.firstName,
+            lastName: parsedBody.lastName
+        });
 
-res.json({
-    message: "Signup Succeeded"
-})
-})
+        res.status(201).json({ message: "User created successfully" });
+
+    } catch (err) {
+        if (err instanceof z.ZodError) {
+            return res.status(400).json({ error: err.errors });
+        }
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
+
+
+
 
 userRouter.post("/signin", function (req, res){
 res.json({
